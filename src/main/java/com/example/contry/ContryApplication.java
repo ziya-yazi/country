05c1e2a95a128c57d8e25f626e02082105249cc8
yaser rasheed <yazerpp@gmail.com>
2026-04-15 12:09:54 +0530

    Initial commit

---
+++ src/main/java/com/example/contry/ContryApplication.java
@@ -0,0 +1,13 @@
+package com.example.contry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContryApplication.class, args);
+	}
+
+}
